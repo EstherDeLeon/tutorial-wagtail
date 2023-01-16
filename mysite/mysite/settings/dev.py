3b24f8b9ded665a54b0b753072f8866e12421c05
unknown <esthercarolinaguzmandeleon@gmail.com>
2023-01-16 11:52:23 +0100

    first commit

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-q7pvfl6)4k3po8%(aloxn-0$i&s4pu)%^y)dyqna(l#0^@_gca"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
