d731b03172e5a2330e254e62c95c97eba7d6477e
unknown <esthercarolinaguzmandeleon@gmail.com>
2023-01-25 11:10:02 +0100

    tags

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,54 +1,63 @@
+from wagtail.models import Page, Orderable
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.search import index
 from django.db import models
 
-# New imports added for ParentalKey, Orderable, InlinePanel
+# Nuevos imports para ClusterTaggableManager, TaggedItemBase, MultiFieldPanel
+
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
 
 from modelcluster.fields import ParentalKey
 
-from wagtail.models import Page, Orderable
-from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel, InlinePanel
-from wagtail.search import index
+from wagtail.snippets.models import register_snippet
+
+from django import forms
 
 
-# ... (Keep the definition of BlogIndexPage, and update BlogPage:)
 
 class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
 
     def get_context(self, request):
-
         # Update context to include only published posts, ordered by reverse-chron
         context = super().get_context(request)
-        blogpages = self.get_children().live().order_by('-first_published_at')
+        blogpages = self.get_children().live().order_by('-latest_revision')
         context['paginas'] = blogpages
-
         return context
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
 
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
 
-    def main_image(self):
-        gallery_item = self.gallery_images.first()
-        if gallery_item:
-            return gallery_item.image
-        else:
-            return None
-
-    search_fields = Page.search_fields + [
-        index.SearchField('intro'),
-        index.SearchField('body'),
-    ]
+    # ... (Keep the main_image method and search_fields definition)
 
     content_panels = Page.content_panels + [
-        FieldPanel('date'),
+        MultiFieldPanel([
+            FieldPanel('date'),
+            FieldPanel('tags'),
+            FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
+        ], heading="Blog information"),
         FieldPanel('intro'),
         FieldPanel('body'),
         InlinePanel('gallery_images', label="Gallery images"),
     ]
 
 class BlogPageGalleryImage(Orderable):
-    page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='gallery_images')
+    page = ParentalKey(BlogPage, on_delete=models.CASCADE, 
+        related_name='gallery_images')
     image = models.ForeignKey(
         'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
     )
@@ -58,3 +67,46 @@ class BlogPageGalleryImage(Orderable):
         FieldPanel('image'),
         FieldPanel('caption'),
     ]
+
+class BlogTagIndexPage(Page):
+    '''
+    Pagina que muestra todas las etiquetas.
+    Recuerda que tienes que crear una pagina de este tipo en el panel de administracion 
+    con el slug "tags"
+    '''
+
+    def get_context(self, request):
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context['blogpages'] = blogpages
+        return context
+
+
+
+@register_snippet
+class BlogCategory(models.Model):
+    '''Snippet para categorias de blog.
+    Este modelo no hereda de Page.
+    Los Snippets los creamos desde el admin de wagtail en `snippets` o `fragmentos`
+    '''
+    name = models.CharField(max_length=255)
+    icon = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('icon'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'blog categories'
